18c9151cc1e71287bf2a809f0e9115bfef3d7a6b
kyh <qnvnfls@naver.com>
2021-11-16 14:06:24 +0900

    2021-11-16 콜라츠 추측2

--- kyh1685/Test07.java
+++ kyh1685/Test07.java
@@ -2,18 +2,20 @@ public class Test07 {
 	public int solution(long num) {
 		int answer = 0;
 
-		for(int i = 1; 500>=i; i++){
-			if(num % 2 == 0){
-				num = num / 2;
-			}else{
-				num = (num * 3) + 1;
-			}
+		if(num != 1){
+			for(int i = 1; 500>=i; i++){
+				if(num % 2 == 0){
+					num = num / 2;
+				}else{
+					num = (num * 3) + 1;
+				}
 
-			if(i == 500 && num != 1){
-				answer = -1;
-			}else if(num == 1){
-				answer = i;
-				break;
+				if(i == 500 && num != 1){
+					answer = -1;
+				}else if(num == 1){
+					answer = i;
+					break;
+				}
 			}
 		}
 		return answer;
