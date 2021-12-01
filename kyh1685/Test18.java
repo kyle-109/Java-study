01c32ba9ca89b941d0fc61f9f27a8f9dccd91ca6
kyh <qnvnfls@naver.com>
2021-12-01 10:32:15 +0900

    2021-12-01 문자열을 정수로 바꾸기

--- kyh1685/Test18.java
+++ kyh1685/Test18.java
@@ -1,5 +1,22 @@
 public class Test18 {
 	public int solution(String s) {
-		return Integer.parseInt(s);
+
+		int answer = 0;
+		String[] arr = s.split("");
+		String str = "";
+
+		if(arr[0].equals("-")){
+			for(int i = 1; arr.length > i; i++){
+				str += arr[i];
+			}
+			answer = Integer.parseInt(str) * -1;
+		}else{
+			for(int i = 0; arr.length > i; i++){
+				str += arr[i];
+			}
+			answer = Integer.parseInt(str);
+		}
+
+		return answer;
 	}
 }
