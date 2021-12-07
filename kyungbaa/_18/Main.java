6f4df4ce5a9c82bcd9b7232f3b04f93d40db6acf
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2021-12-07 21:31:56 +0900

    21-12-07 별찍기_3

---
+++ kyungbaa/_18/Main.java
@@ -0,0 +1,22 @@
+package _18;
+public class Main {
+	public static void main(String[] args) {
+		int n = 5;
+		 /*
+		 		*
+		 	   **
+		 	  ***
+		 	 ****
+		 	*****
+		 */
+		for (int i = 0; i < n ; i++) {
+			for (int j = 0 ; j < n; j++) {
+				if (i < n - (j + 1))
+					System.out.print(" ");
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
