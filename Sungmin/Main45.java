de63e443f1eff797f974b65f6972cf2d16e245c4
Sungmin <ijbmsm@naver.com>
2022-01-10 16:09:09 +0900

    소수 만들기

---
+++ Sungmin/Main45.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Main45 {
+    public String solution(String[] participant, String[] completion) {
+        String answer ="";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for(String player : completion) hm.put(player, hm.get(player) -1);
+        for(String key : hm.keySet()) {
+            if(hm.get(key) != 0) {
+                answer = key; System.out.println(answer);
+                break;
+            }
+        } return answer;
+    }
+
+
+}
