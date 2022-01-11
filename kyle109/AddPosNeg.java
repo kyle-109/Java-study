fcd51223493fa3670e1f8fd8e25e70d015a73a99
kyle109 <84192139+kyle-109@users.noreply.github.com>
2022-01-11 20:13:18 +0900

    11~13일과제제출

---
+++ kyle109/AddPosNeg.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i<absolutes.length;i++){
+            if(signs[i]==true){
+                answer+=absolutes[i];
+            }
+            else{
+                answer+=(absolutes[i]*-1);
+            }
+        }
+        return answer;
+    }
+}
