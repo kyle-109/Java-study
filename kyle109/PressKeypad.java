e7b75b616aef536f6b766906a989741324158ce4
kyle109 <84192139+kyle-109@users.noreply.github.com>
2022-01-18 02:23:40 +0900

    1월 18일 제출

---
+++ kyle109/PressKeypad.java
@@ -0,0 +1,40 @@
+//내 코드의 문제점: sb를 사용하지 않음. 거리계산 알고리즘에서 누락
+class Solution {
+    public String solution(int[] numbers, String hand) {
+		StringBuilder sb = new StringBuilder();
+		int left=10, right=12;
+		int L_dis, R_dis;
+		for(int n : numbers) {
+			L_dis = 0; R_dis = 0;
+			if(n == 1 || n == 4 || n == 7) {
+				sb.append("L");
+				left = n;
+			
+			}else if(n == 3 || n == 6 || n == 9) {
+				sb.append("R");
+				right = n;
+			
+			}else {
+				if(n==0) n += 11;
+				L_dis = (Math.abs(n-left))/3 + (Math.abs(n-left))%3;
+				R_dis = (Math.abs(right-n))/3 + (Math.abs(right-n))%3;
+				if(L_dis == R_dis) {
+					if(hand.equals("right")) {
+						sb.append("R");
+						right = n;
+					}else {
+						sb.append("L");
+						left = n;
+					}
+				}else if(L_dis > R_dis) {
+					sb.append("R");
+					right = n;
+				}else {
+					sb.append("L");
+					left = n;
+				}
+			}
+		}	
+		return sb.toString();
+    }
+}
